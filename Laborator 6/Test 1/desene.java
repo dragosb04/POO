2d264791d23ad1503be51a5192eb3d8fea2f266d
Dragos Belu <dragosinfinit10@gmail.com>
2024-11-02 16:36:12 +0200

    hmm

---
+++ Laborator 6/Test 1/desene.java
@@ -0,0 +1,82 @@
+class Figura {
+    private float arie;
+
+    public Figura (float arie){
+        this.arie = arie;
+    }
+
+    public float getArie(){
+        return arie;
+    }
+
+    public boolean equals (Object o){
+        if (o instanceof Figura){
+            Figura that = (Figura) o;
+            return that.getArie() == arie;
+        }
+        return false;
+    }
+}
+
+class Desen {
+    private String titlu;
+    private Figura[] figuri = new Figura[1024];
+    private int indexFiguri;
+
+    public Desen (String titlu){
+        this.titlu = titlu;
+    }
+
+    public boolean adaugaFigura (Figura obj){
+        if (indexFiguri < 1024){
+            for (int i = 0; i < indexFiguri; i++){
+                if (obj.equals(figuri[i]))
+                    return false;
+            }
+            
+            figuri[indexFiguri++] = obj;
+            return true;
+        }
+        return false;
+    }
+
+    public String toString (){
+        String result = "Titlu desen: " + titlu + "\n";
+        for (int i = 0; i < indexFiguri; i++){
+            result += "Figura[" + i + "]: \n    Arie: " + figuri[i].getArie() + "\n";
+        }
+        return result;
+    }
+
+    public float medieArie (){
+        if (indexFiguri == 0)
+            return 0;
+        
+        float suma = 0;
+        for (int i = 0; i < indexFiguri; i++)
+            suma+= figuri[i].getArie();
+        
+        return (suma / indexFiguri);
+    }
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        Figura figura1 = new Figura(15.5f);
+        Figura figura2 = new Figura(22.0f);
+        Figura figura3 = new Figura(15.5f);
+        Figura figura4 = new Figura(30.0f);
+
+
+        Desen desen = new Desen("Desen");
+
+        boolean added1 = desen.adaugaFigura(figura1);
+        boolean added2 = desen.adaugaFigura(figura2);
+        boolean added3 = desen.adaugaFigura(figura3); 
+        boolean added4 = desen.adaugaFigura(figura4);
+
+        System.out.println(desen);
+        System.out.println("Media arie: " + desen.medieArie());
+    }
+}
